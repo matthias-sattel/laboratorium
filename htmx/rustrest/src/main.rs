65f1a0d48ac1164e18a0bb82dc148166b19c2f24
Matthias Sattel <matthias.sattel@gmail.com>
2025-01-06 15:40:02 +0100

    remove unused method and change path

--- htmx/rustrest/src/main.rs
+++ htmx/rustrest/src/main.rs
@@ -1,14 +1,9 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-#[get("/")]
+#[get("/api")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello World")
 }
-
-#[post("/echo")]
-async fn echo(req_body) -> impl Responder {
-    HttpResponse::Ok.body(req_body)
-} 
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
