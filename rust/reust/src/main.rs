a94b67476ed7d15aa60fb00ea377e1462ef02c07
Matthias Sattel <matthias.sattel@gmail.com>
2025-01-08 23:13:23 +0100

    exeperimenting with actic web framework in Rust

---
+++ rust/reust/src/main.rs
@@ -0,0 +1,17 @@
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/api/hello")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello World")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
